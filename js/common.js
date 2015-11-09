4dcadc4f1ee48cda2e0a1a706c33d893e1af8189
fly <fly@load-page.com>
2015-11-09 18:08:59 +0800

    add print_r to common.js

--- js/common.js
+++ js/common.js
@@ -90,6 +90,53 @@ if (!String.prototype.repeat) {
 		return str;
 	};
 }
+function print_r(array, return_val) {
+	var output = '',
+	pad_char = ' ',
+	pad_val = 4,
+	d = this.window.document,
+	getFuncName = function(fn) {
+		var name = (/\W*function\s+([\w\$]+)\s*\(/).exec(fn);
+		if (!name) return '(Anonymous)';
+		return name[1];
+	};
+	repeat_char = function(len, pad_char) {
+		var str = '';
+		for (var i = 0; i < len; i++) 
+			str += pad_char;
+		return str;
+	};
+	formatArray = function(obj, cur_depth, pad_val, pad_char) {
+		if (cur_depth > 0)
+			cur_depth++;
+
+		var base_pad = repeat_char(pad_val * cur_depth, pad_char);
+		var thick_pad = repeat_char(pad_val * (cur_depth + 1), pad_char);
+		var str = '';
+
+		if (typeof obj === 'object' && obj !== null && obj.constructor && getFuncName(obj.constructor) !== 'PHPJS_Resource') {
+			str += 'Array\n' + base_pad + '(\n';
+			for (var key in obj) {
+				if (Object.prototype.toString.call(obj[key]) === '[object Array]')
+					str += thick_pad + '[' + key + '] => ' + formatArray(obj[key], cur_depth + 1, pad_val, pad_char);
+				else 
+					str += thick_pad + '[' + key + '] => ' + obj[key] + '\n';
+			}
+			str += base_pad + ')\n';
+		} else if (obj === null || obj === undefined)
+			str = '';
+		else // for our "resource" class
+			str = obj.toString();
+		return str;
+	};
+
+	output = formatArray(array, 0, pad_val, pad_char);
+	if (return_val !== true) {
+		alert(output);
+		return true;
+	}
+	return output;
+}
 String.prototype.toHTML = function() {
 	return this.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#039;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 };
