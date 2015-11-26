582ecc5bf95544dbd8eb92a1a43f4699115328e2
fly <fly@load-page.com>
2015-11-26 22:45:01 +0800

    upgrade angular/common.js

--- js/angular/common.js
+++ js/angular/common.js
@@ -39,20 +39,25 @@ angular.module('jquery.ajax', []).provider("$ajax", function(){
 	//this.$get.$inject = ['$rootScope'];
 	//return this;
 });
+angular.module('untils', [])
 //nl2br
-angular.module('untils', []).filter('nl2br', function($sce) {
+.filter('nl2br', function($sce) {
 	return function(input) {
 		if (input !== void 0) {
 			return $sce.trustAsHtml(input.replace(/\n/g, '<br>'));
 		}
 	};
-}).filter('byte2size', function() {
+})
+//byte2size
+.filter('byte2size', function() {
 	return function(input) {
 		if (input !== void 0) {
 			return bytesToSize(input);
 		}
 	};
-}).filter("trustUrl", ['$sce', function ($sce) {
+})
+//trustUrl for video/audio
+.filter("trustUrl", ['$sce', function ($sce) {
 	return function (recordingUrl) {
 		return $sce.trustAsResourceUrl(recordingUrl);
 	};
@@ -61,7 +66,9 @@ var $app = angular.module('app', ['jquery.ajax', 'ui.bootstrap', 'untils']).run(
 	//$http.defaults.headers.common.method = 'get';
 	$rootScope.load = function(page, filters, orders) {};
 	$rootScope.reload = function() {};
-}).directive('a', function() {
+})
+//<a href="空/#" ng-click=""></a>
+.directive('a', function() {
 	return {
 		restrict: 'E',
 		link: function(scope, elem, attrs) {
@@ -69,12 +76,26 @@ var $app = angular.module('app', ['jquery.ajax', 'ui.bootstrap', 'untils']).run(
 				elem.on('click', function(e){
 					e.preventDefault();
 				});
-			
-			if (attrs.method)
-				elem.query(function(json){
-					if (json.result == 'success' && attrs.ngSuccess);
-						
-				}, !attrs.noAlert);
 		}
 	};
+})
+//<a method></a> <form method></form>
+.controller('queryController',  function(){
+
+}).directive('method', function(){
+	return {
+		restrict: 'A',
+		require: '?a',
+		scope: {
+			'done': '&ondone',
+			'fail': '&onfail'
+		},
+		controller: 'queryController',
+		link: function(scope, elem, attrs) {
+			elem.query(function(json){
+				if (json.result == 'success')
+					scope.done.apply(scope);
+			}, !attrs.noAlert);
+		}
+	}
 });
